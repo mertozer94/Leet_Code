e9fe7ce46d26aaa10e810e6d3dbda7001e1bc7d8
Mert Ozer <mertozer94@gmail.com>
2018-02-28 23:57:32 +0100

    Added question no 541

--- src/Reverse_String_541.java
+++ src/Reverse_String_541.java
@@ -7,20 +7,26 @@ public class Reverse_String_541 {
     {
         Reverse_String_541 solution = new Reverse_String_541();
 
-        System.out.println(solution.reverseStr("abcdefg", 2));
+        System.out.println(solution.reverseStr("hyzqyljrnigxvdtneasepfahmtyhlohwxmkqcdfehybknvdmfrfvtbsovjbdhevlfxpdaovjgunjqlimjkfnqcqnajmebeddqsgl",39));
+        //System.out.println(solution.reverseStr("abcdefqa",3));
 
     }
     public String reverseStr(String s, int k) {
 
-        if (s.length() < k)
-            return reverse(s);
+
+        for (int i = 0; i < s.length(); i += 2 * k){
+
+            if (s.substring(i).length() < k)
+                s = s.substring(0, i) + reverse(s.substring(i)) ;
+
+            else
+                s = s.substring(0, i) + reverse(s.substring(i, i + k)) + s.substring(i + k);
+        }
+        return s;
+
     }
     public static String reverse(String s){
-        String newStr = "";
-        for (int i = s.length()-1 ; i >= 0; i--){
-            newStr += s.charAt(i);
-        }
-        return newStr;
+        return new StringBuilder(s).reverse().toString();
     }
 
 }
