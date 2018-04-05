9f87d49f8aabfd173e85270f0d016b691fc5bbfe
Mert Ozer <mertozer94@gmail.com>
2018-04-05 23:10:13 +0200

    Added solutions

---
+++ src/Classes_More_Than_5_Students_596.java
@@ -0,0 +1,13 @@
+/**
+ * @author Mert Ozer
+ */
+public class Classes_More_Than_5_Students_596 {
+    /*
+    *
+    Select class
+    From courses
+    Group by class
+    having count(distinct student) >= 5;
+    *
+    */
+}
--- src/Valid_Palindrome_II_680.java
+++ src/Valid_Palindrome_II_680.java
@@ -1,5 +1,35 @@
 /**
  * @author Mert Ozer
+ * Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
+
+Example 1:
+Input: "aba"
+Output: True
+Example 2:
+Input: "abca"
+Output: True
+Explanation: You could delete the character 'c'.
+Note:
+The string will only contain lowercase characters a-z. The maximum length of the string is 50000.
  */
 public class Valid_Palindrome_II_680 {
+    public static void main(String[] args){
+        System.out.println(validPalindrome("abca"));
+    }
+    public static boolean validPalindrome(String s) {
+        int l = -1;
+        int r = s.length();
+        while (++l < --r){
+            if (s.charAt(l) != s.charAt(r))
+                return isPalindromic(s, l, r + 1) || isPalindromic(s, l - 1, r);
+        }
+        return true;
+    }
+    public static boolean isPalindromic(String s, int l, int r) {
+        while (++l < --r){
+            if (s.charAt(l) != s.charAt(r))
+                return false;
+        }
+        return true;
+    }
 }
